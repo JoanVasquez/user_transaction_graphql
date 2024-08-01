837fd0d38ef1f7227768accfaba23aa124f09eee
Joan Vasquez <dev.joanvasquez@gmail.com>
2024-08-01 11:57:13 -0400

    fix: setting up graphql for the BE

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import dotenv from "dotenv";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import mergedResolvers from "./resolvers/index.js";
+import mergedTypeDefs from "./typeDefs/index.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs: mergedTypeDefs,
+  resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer],
+});
+
+await server.start();
+
+app.use(
+  "/",
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req }),
+  })
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`Server ready at http://localhost:4000/`);
---
+++ backend/resolvers/user.resolver.js
@@ -0,0 +1,12 @@
+import { users } from "../dummyData/data.js";
+
+const userResolver = {
+  Query: {
+    users: () => {
+      return users;
+    },
+    user: (_, { userId }) => users.find((user) => user._id === userId),
+  },
+};
+
+export default userResolver;
