d1664d97b6a2e16c962c291f5a346f43bf8010d9
Joan Vasquez <dev.joanvasquez@gmail.com>
2024-08-02 23:04:28 -0400

    fix: completed FE

---
+++ frontend/src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import GridBackground from "./components/ui/GridBackground.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <GridBackground>
+        <App />
+      </GridBackground>
+    </BrowserRouter>
+  </React.StrictMode>
+);
