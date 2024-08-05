3d3e0663698cba6608b59e79155c4dde6d780f9f
Joan Vasquez <dev.joanvasquez@gmail.com>
2024-08-05 10:16:49 -0400

    fix: preparing for prod deployment

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import passport from "passport";
 import session from "express-session";
 import connectMongo from "connect-mongodb-session";
@@ -18,6 +19,7 @@ import { passportConfig } from "./passport/passport.config.js";
 dotenv.config();
 passportConfig();
 
+const __dirname = path.resolve();
 const app = express();
 app.use(
   cors({
@@ -66,6 +68,13 @@ app.use(
     context: async ({ req, res }) => buildContext({ req, res }),
   })
 );
+
+// Load the optimized frontend from the backend
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+});
 
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
 await connectDB();
